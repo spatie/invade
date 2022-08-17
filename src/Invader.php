cde161cc55322cdf3789d7b6ff06f048104022d8
Deeka Wong <huangdijia@gmail.com>
2022-08-17 13:38:17 +0800

    Closure instead of Reflection

--- src/Invader.php
+++ src/Invader.php
@@ -2,8 +2,6 @@
 
 namespace Spatie\Invade;
 
-use ReflectionClass;
-
 /**
  * @template T of object
  * @mixin T
@@ -12,7 +10,6 @@ class Invader
 {
     /** @var T */
     public object $obj;
-    public ReflectionClass $reflected;
 
     /**
      * @param T $obj
@@ -20,33 +17,20 @@ class Invader
     public function __construct(object $obj)
     {
         $this->obj = $obj;
-        $this->reflected = new ReflectionClass($obj);
     }
 
     public function __get(string $name): mixed
     {
-        $property = $this->reflected->getProperty($name);
-
-        $property->setAccessible(true);
-
-        return $property->getValue($this->obj);
+        return (fn () => $this->{$name})->call($this->obj);
     }
 
     public function __set(string $name, mixed $value): void
     {
-        $property = $this->reflected->getProperty($name);
-
-        $property->setAccessible(true);
-
-        $property->setValue($this->obj, $value);
+        (fn () => $this->{$name} = $value)->call($this->obj);
     }
 
     public function __call(string $name, array $params = []): mixed
     {
-        $method = $this->reflected->getMethod($name);
-
-        $method->setAccessible(true);
-
-        return $method->invoke($this->obj, ...$params);
+        return (fn () => $this->{$name}(...$params))->call($this->obj);
     }
 }
