2a8c9d6a016c18f3c3b36fe6396ddec13b8ec7aa
Deeka Wong <huangdijia@gmail.com>
2022-08-17 13:38:25 +0800

    Fix test

--- tests/FooTest.php
+++ tests/FooTest.php
@@ -15,7 +15,7 @@ it('can read a private property of an object', function () {
     $privateValue = invade($this->bar)->privateProperty;
 
     expect($privateValue)->toBe('privateValue');
-})->expectExceptionMessage('Property Bar::$privateProperty does not exist');
+})->expectExceptionMessage('Undefined property: Bar::$privateProperty');
 
 it('can set the private property of an object', function () {
     invade($this->bar)->privateProperty = 'changedValue';
@@ -23,13 +23,13 @@ it('can set the private property of an object', function () {
     $privateValue = invade($this->bar)->privateProperty;
 
     expect($privateValue)->toBe('changedValue');
-})->expectExceptionMessage('Property Bar::$privateProperty does not exist');
+});
 
 it('can call the private method of an object', function () {
     $returnValue = invade($this->bar)->privateMethod();
 
     expect($returnValue)->toBe('private return value');
-});
+})->expectErrorMessage('Call to private method Foo::privateMethod() from scope Bar');
 
 class Foo
 {
